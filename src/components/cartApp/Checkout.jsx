34051be479333484f951f11db73f7198d82379d3
MRumeaux <mrumeaux@gmail.com>
2025-06-03 12:43:05 -0300

    sumando-pedido-a-firebase

--- src/components/cartApp/Checkout.jsx
+++ src/components/cartApp/Checkout.jsx
@@ -1,7 +1,40 @@
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
+import { useForm } from "react-hook-form";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../../firebase/config";
 
 const Checkout = () => {
+
+    const {carrito, precioTotal, vaciarCart} = useContext(CartContext);
+
+    const {register, handleSubmit} = useForm();
+    
+    const compraForm = (data) => {
+        const pedido = {
+            cliente: data,
+            productos: carrito,
+            totalCarrito: precioTotal()
+        }
+
+        const pedidosRef = collection(db, "pedidos");
+        addDoc(pedidosRef, pedido)
+    }
+
     return (
-        <div>Checkout</div>
+        <div className="posicion-contenedor ">
+            <div className="contenido-tienda">
+                <h1 className="titulo-principal">Para finalizar la compra te pediremos que completes tus datos de contacto</h1>
+                <form className="form" onSubmit={handleSubmit(compraForm)}>
+
+                    <input type="text" placeholder="Ingresá tu nombre" {...register("nombre")} />
+                    <input type="email" placeholder="Ingresá tu email" {...register("email")} />
+                    <input type="phone" placeholder="Ingresá tu número de teléfono" {...register("telefono")} />
+
+                    <button type="submit" className="envio-form">Confirmar y comprar ahora</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
