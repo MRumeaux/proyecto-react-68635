4bf60f29ab466110f8adfed4310a7fa013b9f4f5
MRumeaux <mrumeaux@gmail.com>
2025-06-04 00:07:20 -0300

    reversion-stock

--- src/components/cartApp/Checkout.jsx
+++ src/components/cartApp/Checkout.jsx
@@ -1,14 +1,14 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "../../context/CartContext";
 import { useForm } from "react-hook-form";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, writeBatch } from "firebase/firestore";
 import { db } from "../../firebase/config";
 
 const Checkout = () => {
 
     const {carrito, precioTotal, vaciarCart} = useContext(CartContext);
-
     const {register, handleSubmit} = useForm();
+    const [pedidoId, setPedidoId] = useState("");
     
     const compraForm = (data) => {
         const pedido = {
@@ -18,10 +18,33 @@ const Checkout = () => {
         }
 
         const pedidosRef = collection(db, "pedidos");
+
+        const actualizarStock = async (pedido) => {
+            const batch = writeBatch(db);
+            batch.set(pedidosRef, pedido);
+            pedido.productos.forEach(prod => {
+                const producto = doc(db, "productos", prod.id)
+                batch.update(producto, {stock: producto.stock - producto.cantidad})
+                });
+            await batch.commit();
+        }
+
         addDoc(pedidosRef, pedido)
             .then((doc) => { 
-                console.log(doc.id)
+                setPedidoId(doc.id);
+                actualizarStock();
+                vaciarCart();
             })
+        
+    }
+
+    if (pedidoId) {
+        return (
+            <div className="contenido-tienda">
+                <h1 className="titulo-principal">¡Compra realizada con éxito!</h1>
+                <p>Tu número de pedido es: {pedidoId}</p>
+            </div>
+        )
     }
 
     return (
